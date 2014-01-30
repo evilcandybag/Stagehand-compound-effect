bea103da3309a8f3e2cb4b2b8a70a2882280a7ce
Peter Holm <peter@insella.se>
2014-01-30 15:46:45 +0100

    Added effect prototype

---
+++ src/main/scala/se/stagehand/effects/compound/Effect.scala
@@ -0,0 +1,12 @@
+package ${packageId}
+
+import se.stagehand.lib.scripting._
+import scala.xml.Elem
+import scala.swing._
+
+class Stagehand-compound-effectEffect extends Effect {
+  def componentName = "Stagehand-compound-effect"
+    
+  def trigger = {}
+
+}
---
+++ src/main/scala/se/stagehand/effects/compound/Plugin.scala
@@ -0,0 +1,13 @@
+package ${packageId}
+
+import se.stagehand.plugins.EffectPlugin
+import se.stagehand.lib.scripting.Effect
+
+class Stagehand-compound-effectPlugin extends EffectPlugin {
+
+  val name = "Stagehand-compound-effect"
+  
+  val guis: List[ComponentGUI] = List()
+  val effects: Array[Effect] = Array()
+  
+}
